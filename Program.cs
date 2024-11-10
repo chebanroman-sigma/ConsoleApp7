4a13a29cbee3e439f8374f148a356c6e3464a8c9
chebanroman-sigma <r3388967@gmail.com>
2024-11-10 18:19:40 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp6
+    
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //zadanie 1
+            //Console.Write("Ваше Имя: ");
+            //string name1 = Console.ReadLine();
+            //Console.WriteLine(a);
+            //Console.Write("Ваше Имя: ");
+            //string name2 = Console.ReadLine();
+            //Console.WriteLine($"Привет {b}!");
+            //Console.ReadKey();
+
+
+            //zadanie 2 
+            //Console.Write("Введите название любимой футбольной команды: ");
+            //string comanda = Console.ReadLine();
+            //Console.Write($"{comanda}-  это чемпион!");
+            //Console.ReadKey();
+
+
+            //zadanie 3
+            //Console.Write("Ведите ваше имя");
+            //string name = Console.ReadLine();
+            //Console.Write("Ведите вашу фамилию");
+            //string surname = Console.ReadLine();
+            //Console.Write($"{ name} {surname}");
+            //Console.ReadKey();
+
+
+            //zadanie 4 
+            //Console.Write("Введите  государство: ");
+            //string country =   Console.ReadLine();
+            //Console.Write("введите столицу");
+            //string capital =  Console.ReadLine();
+            //Console.Write($"Столица государства {country} - город {capital}");
+
+
+            //zadanie  5
+            //Console.Write("ведите название романа: ");
+            // string poema =  Console.ReadLine();
+            //Console.Write("напишите фамилию  автора этого романа: ");
+            // string surname = Console.ReadLine();
+            //Console.Write($"Писатель {surname} - автор романа {poema} ");
+            //Console.ReadKey();
+
+
+            //zadanie 6
+            //Console.Write("Введите название футбольного клуба: ");
+            //string footclub = Console.ReadLine();
+            //Console.Write($"количество символов: {footclub.Length}");
+            //Console.ReadKey();
+
+
+            //zadanie 7
+            //Console.WriteLine("Ты вчера был болен.");
+            //Console.WriteLine("Измерь - ка  температуру!");
+            //Console.Write("Сообщи, какая у тебя  температура: ");
+            //double temp = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Ты здоров дружок!");
+            //Console.WriteLine("Можешь идти в школу.");
+            //Console.WriteLine("Желаю успехов!");
+            //Console.ReadKey();
+
+
+            //zadanie 8
+            //Console.Write("Здравствуй, я компьютер, а тебя как зовут? ");
+            //string name =  Console.ReadLine();
+            //Console.Write($"Очень приятно, {name}.");
+            //Console.Write("Сколько тебе лет? ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.Write($"Ого! Целых {age} лет! Ты уже совсем взрослый!");
+            //Console.ReadKey();
+
+
+            //zadanie 9
+            //Console.Write("Как тебя зовут? ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Привет,  {name}!");
+            //Console.Write("Сколько тебе лет? ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.Write("Какой у тебя любимый предмет в техникуме? ");
+            //string lesson = Console.ReadLine();
+            //Console.Write("Какая у тебя любимая книга? ");
+            //string book = Console.ReadLine();
+            //Console.WriteLine($"Значит тебя зовут, {name}.");
+            //Console.WriteLine($"Тебе {age} лет.");
+            //Console.WriteLine($"Твой любимый предмет - {lesson}.");
+            //Console.WriteLine($"И твоя любимая книга – { book}");
+            //Console.ReadKey();
+
+
+            //zadanie 10
+            //Console.Write("Ваше имя: ");
+            //string name = Console.ReadLine();
+            //Console.Write("сколько вам лет? ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{name}, ты родился в {2024-age} году. ");
+            //Console.ReadKey();
+
+
+
+
+        }
+
+    }
+}
